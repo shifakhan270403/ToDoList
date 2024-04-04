7c6928c66179d94fb321023cb5efc6c04fbd323a
shifakhan270403 <shifakhan.work@gmail.com>
2024-04-05 02:00:38 +0530

    Initial Basic Code

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Create(){
+    return(
+        <div>
+            <input type="text" name="" id="" />
+            <button type="button">ADD</button>
+            
+        </div>
+    )
+}
+
+export default Create
