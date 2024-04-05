4ee5b7ae7f8f5df68607c9054a620713c6005a02
shifakhan270403 <shifakhan.work@gmail.com>
2024-04-06 00:55:31 +0530

    Added backend files and made a succesful DB connection on MongoDB

--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react"
+import axios from 'axios'
 
 function Create(){
+    const [task, setTask] = useState()
+
+    const handleAdd =()=>{
+        axios.post('http://localhost:3001/add',{task:task})
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
     return(
-        <div>
-            <input type="text" name="" id="" />
-            <button type="button">ADD</button>
-            
+        <div class="create_form">
+            <input type="text" placeholder="Enter Task" onChange={(e) => setTask(e.target.value)} />
+            <button type="button" onCLick={handleAdd}>ADD</button>
         </div>
     )
 }
--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
+import Create from './Create'
 
 function Home(){
     const [todos, setTodos] = useState([])
     return(
-    <div>
+    <div class='home'>
         <h2>Todo List</h2>
         <Create />
         {   
-            todos.length === 0 ?
-            <div>
-                <h2>NO RECORD</h2>
-            </div>
+            todos.length === 0 
+            ?
+            <div><h2>NO RECORD</h2></div>
             :
             todos.map(todo => (
                 <div>
